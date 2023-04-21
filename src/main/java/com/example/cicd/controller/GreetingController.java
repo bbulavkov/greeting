96b2bdd905b3bcc7332100e5c36863701e243b4e
bohdan <bohdan.bulavkov@gmail.com>
2023-04-21 18:46:54 +0300

    Update

--- src/main/java/com/example/cicd/controller/GreetingController.java
+++ src/main/java/com/example/cicd/controller/GreetingController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
     @GetMapping
     public String greet() {
-        return "Hello world";
+        return "Hello world!!!";
     }
 }
